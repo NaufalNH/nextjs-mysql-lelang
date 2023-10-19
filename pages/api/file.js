d7e4fd4c39a5c38c5e8155439cd43703ce473c87
NaufalNH <akuntumbalnopal2@gmail.com>
2023-10-19 19:36:00 +0700

    add fitur update barang

--- pages/api/file.js
+++ pages/api/file.js
@@ -18,27 +18,63 @@ export default async (req, res) => {
             
             form.parse(req, async (err, fields, files)  => {
                  if (err) return reject(err)
-            if (fields.token[0]) {
-                const select = await sqlconnect({
-                    query: "SELECT `level` FROM user WHERE `token` = ?  ",
-                    values: [fields.token[0]],
-                });
-                if (select[0]?.level === "admin" || select[0]?.level === "petugas") {
-                    var oldPath = files.file[0].filepath;
-                 var newPath = `./public/upload/${files.file[0].originalFilename}`;
-                 mv(oldPath, newPath, function(err) {
-                 });
-                    const hasil = await sqlconnect({
-                        query: "INSERT INTO `barang` SET `nama_barang` = ? , `harga_awal` = ? , `deskripsi` = ? , `image` = ? ",
-                        values: [fields.nama_barang[0], fields.harga_awal[0], fields.deskripsi[0] , files.file[0].originalFilename],
-                    });
-                    res.status(200).json({ fields, files, coba:files.file[0] , token:fields.token[0] , response:"Berhasil Menambahkan!" })
-                }else{
-                    res.status(401).json({response:"Unauthorized"})
-                }
-                }else{
-                    res.status(401).json({response:"Token needed!"})
-                }
+                 if (fields.method[0] === "tambah") {
+                    if (fields.token[0]) {
+                        const select = await sqlconnect({
+                            query: "SELECT `level` FROM user WHERE `token` = ?  ",
+                            values: [fields.token[0]],
+                        });
+                        if (select[0]?.level === "admin" || select[0]?.level === "petugas") {
+                            var oldPath = files.file[0].filepath;
+                         var newPath = `./public/upload/${files.file[0].originalFilename}`;
+                         mv(oldPath, newPath, function(err) {
+                         });
+                            const hasil = await sqlconnect({
+                                query: "INSERT INTO `barang` SET `nama_barang` = ? , `harga_awal` = ? , `deskripsi` = ? , `image` = ? ",
+                                values: [fields.nama_barang[0], fields.harga_awal[0], fields.deskripsi[0] , files.file[0].originalFilename],
+                            });
+                            res.status(200).json({ fields, files, coba:files.file[0] , token:fields.token[0] , response:"Berhasil Menambahkan!" })
+                        }else{
+                            res.status(401).json({response:"Unauthorized"})
+                        }
+                        }else{
+                            res.status(401).json({response:"Token needed!"})
+                        }
+                 }
+                 if (fields.method[0] === "update") {
+                    if (fields.token[0]) {
+                        const select = await sqlconnect({
+                            query: "SELECT `level` FROM user WHERE `token` = ?  ",
+                            values: [fields.token[0]],
+                        });
+                        if (select[0]?.level === "admin" || select[0]?.level === "petugas") {
+                            if (files.file) {
+                                var oldPath = files.file[0].filepath;
+                                var newPath = `./public/upload/${files.file[0].originalFilename}`;
+                                mv(oldPath, newPath, function(err) {
+                                });
+                                   const hasil = await sqlconnect({
+                                       query: "UPDATE `barang` SET `nama_barang` = ? , `harga_awal` = ? , `deskripsi` = ? , `image` = ? WHERE id_barang = ? ",
+                                       values: [fields.nama_barang[0], fields.harga_awal[0], fields.deskripsi[0] , files.file[0].originalFilename , fields.id_barang[0]],
+                                   });
+                                   res.status(200).json({ fields, files, coba:files.file[0] , token:fields.token[0] , response:"Berhasil Menambahkan!" })
+                            } 
+                            else {
+                                const hasil = await sqlconnect({
+                                    query: "UPDATE `barang` SET `nama_barang` = ? , `harga_awal` = ? , `deskripsi` = ? WHERE id_barang = ? ",
+                                    values: [fields.nama_barang[0], fields.harga_awal[0], fields.deskripsi[0] , fields.id_barang[0]],
+                                });
+                                res.status(200).json({ fields, files , token:fields.token[0] , response:"Berhasil Menambahkan!" })
+                            }
+                          
+                        }else{
+                            res.status(401).json({response:"Unauthorized"})
+                        }
+                        }else{
+                            res.status(401).json({response:"Token needed!"})
+                        }
+                 }
+           
              })
          })
          
--- pages/main/barang.js
+++ pages/main/barang.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { Button, Pagination as Pagination2, Avatar, Dialog, DialogContent, DialogContentText, DialogActions, Modal } from "@mui/material";
-import {Delete, Add} from "@mui/icons-material";
+import {Delete, Add, Edit} from "@mui/icons-material";
 import Image from "next/image";
 import { rupiah } from "./[id]";
 
@@ -10,14 +10,13 @@ function Tambah(params) {
 const [open, setOpen] = useState(false);
 const [data,setData] = useState({
   token: params?.token,
-//   method:"tambah",
+  method:"tambah",
   nama_barang:"",
   harga_awal:"",
   deskripsi:""
 })
 const [image, setImage] = useState(null);
 const [createObjectURL, setCreateObjectURL] = useState(null);
-console.log(createObjectURL)
 const router = useRouter();
 const handleChange = (event) => {
   const { name, value } = event.target;
@@ -37,7 +36,7 @@ const handleClik = async (event) => {
     formData.append("nama_barang", data?.nama_barang);  
     formData.append("harga_awal", data?.harga_awal);  
     formData.append("deskripsi", data?.deskripsi);  
-    console.log(formData)
+    formData.append("method", data?.method);  
     try {
       const send = await axios({
         method:"POST",
@@ -81,6 +80,88 @@ const handleClik = async (event) => {
   <div className="flex items-center gap-3" style={{justifyContent:"right"}}>
   <Button variant="contained" color="error" className="bg-red-600" onClick={() => {setOpen(false)}}>Cancel</Button> 
   <Button variant="contained" className="bg-blue-600" onClick={handleClik}>Submit</Button> 
+  </div>
+  </div>
+      </Modal>
+      </>
+    )
+}
+function UpdateBarang({token, nama , id , deskripsi , harga_awal, imagename}) {
+const [open, setOpen] = useState(false);
+const [data,setData] = useState({
+  token: token,
+  method:"update",
+  nama_barang: nama,
+  harga_awal:harga_awal,
+  deskripsi: deskripsi,
+  id_barang: id
+})
+const [image, setImage] = useState(null);
+const [createObjectURL, setCreateObjectURL] = useState(null);
+const router = useRouter();
+const handleChange = (event) => {
+  const { name, value } = event.target;
+  if (name === "image") {
+    setImage(event.target.files[0]);
+    setCreateObjectURL(URL.createObjectURL(event.target.files[0]));
+  } else {
+    setData({ ...data, [name]: value, token:token });
+  }
+};
+
+const handleClik = async (event) => {    
+    event.preventDefault();
+    const formData = new FormData();
+      formData.append("file", image);
+    formData.append("token", data?.token);  
+    formData.append("nama_barang", data?.nama_barang);  
+    formData.append("harga_awal", data?.harga_awal);  
+    formData.append("deskripsi", data?.deskripsi);  
+    formData.append("method", data?.method);  
+    formData.append("id_barang", data?.id_barang);  
+    try {
+      const send = await axios({
+        method:"POST",
+        url:"/api/file",
+        data:formData,
+        headers:{"Content-Type" : "multipart/form-data"}
+      })
+      if (send.data.response) {
+        router.reload()
+      }
+    } catch (error) {
+      console.log(error)
+    }   
+    
+  };
+
+    return(
+        <>
+        <Button className="bg-blue-600 mb-2 flex" color="primary" variant="contained" size="medium" onClick={() => {setOpen(true)}}><Edit /></Button>
+        <Modal
+        open={open}
+        onClose={() => {setOpen(false)}}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        className="flex justify-center items-center"
+      >
+<div className="bg-white w-[auto] h-[auto] rounded-lg border-black p-[16px] flex flex-col gap-6">
+  <h1 className="text-lg font-bold">Edit Barang</h1>
+  <div className="flex flex-row gap-3 items-center">
+  <label>Nama Barang : </label> <input type="text" name="nama_barang" value={data?.nama_barang} onChange={handleChange} className="border-black bg-gray-200 rounded-md p-[8px]" placeholder="Nama Barang" ></input> 
+  </div>
+  <div className="flex flex-row gap-3 mb-2">
+  <div className="gap-[61px] flex"><label>Image </label> <label>:</label></div>  <div className="flex flex-col gap-3"> <input type="file" name="image" onChange={handleChange} ></input>{ imagename && createObjectURL === null ? <Image src={"/upload/" + imagename} width={200} height={200} className="object-contain max-h-[10rem]" />: createObjectURL !== null ? <Image src={createObjectURL} width={200} height={200} className="object-contain max-h-[10rem]" /> : <Image src="/assets/images/notfound.png" width={200} height={200} className="object-contain max-h-[10rem]" />}</div>
+  </div>
+  <div className="flex flex-row gap-3">
+  <label>Harga Barang : </label> <div className="flex flex-col gap-3"> <input type="number" name="harga_awal" value={data?.harga_awal} onChange={handleChange} className="border-black bg-gray-200 rounded-md p-[8px]" placeholder={rupiah(data?.harga_awal)} ></input> <p className="text-xs text-gray-500">{rupiah(data?.harga_awal)}</p> </div>
+  </div>
+  <div className="flex flex-row gap-3 items-center mb-2">
+  <div className="gap-10 flex"><label>Deskripsi </label> <label>:</label></div> <textarea type="text" name="deskripsi" value={data?.deskripsi} onChange={handleChange} className="border-black bg-gray-200 rounded-md p-[8px]" placeholder="Deskripsi barang" ></textarea> 
+  </div>
+  <div className="flex items-center gap-3" style={{justifyContent:"right"}}>
+  <Button variant="contained" color="error" className="bg-red-600" onClick={() => {setOpen(false)}}>Cancel</Button> 
+  <Button variant="contained" className="bg-blue-600" onClick={handleClik}>Update</Button> 
   </div>
   </div>
       </Modal>
@@ -301,6 +382,7 @@ useEffect(() => {
                 <td class=" pl-6 py-4">
                     <div className="gap-2 flex">
                    {user?.level === "petugas" && data?.status === "ada" ? <Tambahlelang id={data?.id_barang} token={user?.token} nama={data?.nama_barang} harga_awal={data?.harga_awal} deskripsi={data?.deskripsi} image={data?.image} status={data?.status} /> : user?.level === "petugas" ? <Button disabled className="bg-green-600 mb-2 flex" color="success" variant="contained" size="medium"><Add /></Button> : ""} 
+                   <UpdateBarang id={data?.id_barang} token={user?.token} nama={data?.nama_barang} harga_awal={data?.harga_awal} deskripsi={data?.deskripsi} imagename={data?.image} status={data?.status} />
                     <Deletebarang id={data?.id_barang} token={user?.token} nama={data?.nama_barang}/>
                     </div>
                 </td>
@@ -342,7 +424,6 @@ export const Laporan = () => {
         // const get = await axios.get("/api/pengaduan")
         if(user.level === "admin" || user.level === "petugas")
         {
-          console.log("runnninf")
             const get = await axios({
                 method:"POST",
                 url:"/api/lelang",
